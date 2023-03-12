36cc045808c9aac9505994e837618e21e871126f
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-12 23:08:20 +0600

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 5_000;
+        int miles = 20;
+
+        int bonus = price / miles;
+        System.out.println("Количество бонусных миль: " + bonus);
     }
 }
